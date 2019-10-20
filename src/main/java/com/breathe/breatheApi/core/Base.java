03128dbba9ab53707886c429f952fd02ce1941cf
mlashner <lashnem@gmail.com>
2019-10-20 17:35:03 -0500

    Fix hibernate sequence

--- src/main/java/com/breathe/breatheApi/core/Base.java
+++ src/main/java/com/breathe/breatheApi/core/Base.java
@@ -14,8 +14,11 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class Base {
     @Id
-    @GenericGenerator(name = "PER_ENTITY_GENERATOR", strategy = "enhanced-sequence", parameters = {
-            @Parameter(name = "prefer_sequence_per_entity", value = "true")
+    @GenericGenerator(name = "PER_ENTITY_GENERATOR",  strategy = "enhanced-sequence", parameters = {
+            @Parameter(name = "prefer_sequence_per_entity", value = "true"),
+            @Parameter(name = "initial_value", value = "1"),
+            @Parameter(name = "increment_size", value = "1"),
+            @Parameter(name = "optimizer", value = "pooled-lo")
     })
     @GeneratedValue(generator = "PER_ENTITY_GENERATOR")
     @Column(name = "id", updatable = false, nullable = false)
